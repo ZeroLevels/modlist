66c993fe39c180ea1b718a98096e4cb41bbb90fb
Samuel Ryan <sam@samryan.co.uk>
2013-03-21 13:42:37 +0000

    Sensible redirect

---
+++ latest/index.php
@@ -0,0 +1,4 @@
+<?php 
+
+header("HTTP/1.1 302 Moved Temporaryily"); // httpstatus.es/302 -- set temporarily 'cause the link will change again, so people don't cache it
+header("Location: http://modlist.mcf.li/list/1.5/1.5.1.php"); 
