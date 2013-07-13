1fea583b300f1d5b189121f8163ec035b8b907c1
GrygrFlzr <grygrflzr@hotmail.com>
2013-07-13 17:24:25 +0800

    Banner moved to one file

--- list/1.5/1.5.0.php
+++ list/1.5/1.5.0.php
@@ -25,10 +25,7 @@ include('../../resources/scripts/jsontable.php');
 </head>
 <body>
 
-<div id="bannerTop" title="Developer of ComputerCraft along with Dan200 - a mod for Minecraft">
-	<br/><a class="help" href="http://cloudhunter.co.uk/post/51553893017/an-appeal-please-help-me-rescue-my-daughter">&nbsp;Please help Cloudy rescue his daughter!&nbsp;</a>
-	<br/>Visit the above URL to read the details. Please consider making a donation to <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3NL5NDTHGF4FW">Save Vanessa!</a><br/>Keep up to date with the situation by checking out <a href="https://twitter.com/Cloudhunter">Cloudy's Twitter</a> - <a href="https://twitter.com/search?q=%23GetCloudysDaughter&src=hash">#getCloudysdaughter</a>
-</div>
+<?php include '../banner.php'; ?>
 
 <p>
 	<img src="../../resources/images/<?php echo $version; ?>.png" width="127" height="58"/><br/>
