9e6914cb65e16cea5c488b74ae26798c4aa4078c
GrygrFlzr <grygrflzr@hotmail.com>
2013-10-10 22:14:38 +0800

    Re-added Google Analytics, Fixed Imagethread

--- resources/templates/modlist-template.php
+++ resources/templates/modlist-template.php
@@ -108,6 +108,20 @@ foreach ($this->lastjavascripts as $javascript) {
 <!--[if lt IE 9]>
 <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.min.js"></script>
 <![endif]-->
+<!--Google Analytics-->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39433845-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
 <?php
