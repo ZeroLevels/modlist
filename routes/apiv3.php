1bdb40445150ba046161d18aa6506994e62a2845
GrygrFlzr <grygrflzr@hotmail.com>
2013-10-28 12:06:35 +0800

    Completed APIv3

--- routes/apiv3.php
+++ routes/apiv3.php
@@ -3,19 +3,33 @@
 /*
  * api/v3/version.json
  * Return list for specified version
- * @return page
- */
-
-$this->respond('GET', '/[*:version].json', function ($request, $response, $service, $app) {
-    
-});
-
-/*
+ * 
  * api/v3/version.md5
  * Return md5 hash for specified version
+ * 
  * @return page
  */
 
-$this->respond('GET', '/[*:version].md5', function ($request, $response, $service, $app) {
-    
+$this->respond('GET', '/[*:version].[json|md5:filetype]', function ($request, $response, $service, $app) {
+    if($request->param('version') !== '') {
+        $modlist = json_decode(file_get_contents('data/modlist.json'), 1);
+        $newlist = array();
+        
+        if($request->param('version') === 'all') {
+            $newlist = $modlist;
+        } else {
+            foreach($modlist as &$mod) {
+                if(in_array($request->param('version'),$mod['versions'],true)) {
+                    array_push($newlist, $mod);
+                }
+            }
+        }
+        
+        if($request->param('filetype') === 'json') {
+            $response->json($newlist);
+        }
+        if($request->param('filetype') === 'md5') {
+            $response->body(md5(json_encode($newlist)));
+        }
+    }
 });
