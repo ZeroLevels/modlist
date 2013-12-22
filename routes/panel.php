ec9b747d3596b1a1c6c810467031f299231c62c8
GrygrFlzr <grygrflzr@hotmail.com>
2013-12-22 17:14:33 +0800

    Added bit.ly expanding and shortening functionality

--- routes/panel.php
+++ routes/panel.php
@@ -300,3 +300,63 @@ $this->respond('GET', '/submission/[*:id]', function ($request, $response, $serv
         'forgecolor'  => $service->forgecolor
     ));
 });
+
+/*
+ * panel/bitly/save/
+ * Save a new bit.ly link
+ * @return page
+ */
+
+$this->respond('GET', '/bitly/save/[*:link]', function ($request, $response, $service, $app) {
+    if(!$service->permissions->canAccess('panel.submission.bitly.shorten')) {
+        $service->render('html/panel/forbidden.phtml');
+        return;
+    }
+
+    //Submit data to Bit.ly API
+    $json_data = json_decode(file_get_contents(
+            'https://api-ssl.bitly.com/v3/shorten?access_token=' . BITLY_TOKEN .
+            '&longUrl=' . urlencode($request->param('link'))
+            , false), true);
+    
+    $output['link'] = $json_data['data']['url'];
+    
+    //Show data
+    $response->noCache();
+    $response->header('Content-Type', 'application/json');
+    $response->body(json_encode($output, JSON_UNESCAPED_SLASHES));
+});
+
+/*
+ * panel/bitly/info/
+ * Get the info of the bit.ly link
+ * @return page
+ */
+
+$this->respond('GET', '/bitly/info/[*:link]', function ($request, $response, $service, $app) {
+    if(!$service->permissions->canAccess('panel.submission.bitly.expand')) {
+        $service->render('html/panel/forbidden.phtml');
+        return;
+    }
+
+    //Request info from Bit.ly API
+    $info = json_decode(file_get_contents(
+            'https://api-ssl.bitly.com/v3/info?access_token=' . BITLY_TOKEN .
+            '&shortUrl=' . urlencode($request->param('link'))
+            , false), true);
+    
+    $output['title'] = $info['data']['info'][0]['title'];
+    
+    //Request expanded URL from Bit.ly API
+    $expand = json_decode(file_get_contents(
+            'https://api-ssl.bitly.com/v3/expand?access_token=' . BITLY_TOKEN .
+            '&shortUrl=' . urlencode($request->param('link'))
+            , false), true);
+    
+    $output['link'] = $expand['data']['expand'][0]['long_url'];
+    
+    //Show data
+    $response->noCache();
+    $response->header('Content-Type', 'application/json');
+    $response->body(json_encode($output, JSON_UNESCAPED_SLASHES));
+});
