7d1d1d917800e57870854d1d918b79994f002a6f
GrygrFlzr <grygrflzr@hotmail.com>
2014-02-10 23:11:40 +0800

    Updated homepage resize to only apply on medium+ screens

--- public/resources/js/home.js
+++ public/resources/js/home.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    var maxHeight = -1;
-    $('.home-changelog div p:nth-of-type(2)').each(function() {
-        maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-    });
-    $('.home-changelog div p:nth-of-type(2)').each(function() {
-        $(this).height(maxHeight);
-    });
+    if(screen.width >= 768) {
+        var maxHeight = -1;
+        $('.home-changelog div p:nth-of-type(2)').each(function() {
+            maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+        });
+        $('.home-changelog div p:nth-of-type(2)').each(function() {
+            $(this).height(maxHeight);
+        });
+    }
 });
