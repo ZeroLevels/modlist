482daeb9a8f617994d52ad36f59cd99056134137
Samuel Ryan <sam@samryan.co.uk>
2014-08-02 16:41:33 +0100

    Adds version controller: versions list and versio

--- app/Routes.php
+++ app/Routes.php
@@ -1,3 +1,5 @@
 <?php
 
 $router->add('GET', '/', 'HomeController@getIndex');
+$router->add('GET', '/versions', 'VersionsController@getIndex');
+$router->add('GET', '/versions/[:version]', 'VersionsController@getVersion');
