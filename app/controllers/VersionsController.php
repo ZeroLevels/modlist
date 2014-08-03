d5a95194d497fff1b2a5a8f5dcbdb60c64c91af1
GrygrFlzr <grygrflzr@hotmail.com>
2014-08-03 23:03:50 +0800

    Renamed .html files to .twig for proper syntax highlighting

--- app/controllers/VersionsController.php
+++ app/controllers/VersionsController.php
@@ -7,14 +7,14 @@ class VersionsController extends Controller\Twig {
 	public function getIndex()
 	{
 		$versions = Version::all();
-		return $this->make('versions/versions.html', compact('versions'));
+		return $this->make('versions/versions.twig', compact('versions'));
 	}
 
 	public function getVersion($version)
 	{
 		$version = Version::where('version', $version)->first();
 		$mods = $version->mods()->get();
-		return $this->make('versions/version.html', compact('version', 'mods'));
+		return $this->make('versions/version.twig', compact('version', 'mods'));
 	}
 
 }
