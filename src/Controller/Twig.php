de9201e68625b434b6ab59b6c6c97788bf7d2fa9
GrygrFlzr <grygrflzr@hotmail.com>
2014-08-04 00:19:14 +0800

    Pass more metadata to layout

--- src/Controller/Twig.php
+++ src/Controller/Twig.php
@@ -12,7 +12,11 @@ class Twig extends Base {
 	{
 		$loader = new Twig_Loader_Filesystem(__DIR__ . '/../../app/views');
 		$this->twig = new Twig_Environment($loader);
-		$data['uri'] = $this->request->uri();
+		
+		$data['meta']['uri']        = $this->request->uri();
+		$data['meta']['cachetime']  = filemtime(__DIR__ . '/../../public/resources/stylesheets/modlist.css');
+		$data['meta']['cookies']    = $this->request->cookies();
+		
 		return $this->twig->render($view, $data);
 	}
 
