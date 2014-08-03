257d1b0f1a1acb02d95e0a16b10f64d4300a38f8
GrygrFlzr <grygrflzr@hotmail.com>
2014-08-03 23:54:43 +0800

    Pass request URI to all pages

--- src/Controller/Twig.php
+++ src/Controller/Twig.php
@@ -12,6 +12,7 @@ class Twig extends Base {
 	{
 		$loader = new Twig_Loader_Filesystem(__DIR__ . '/../../app/views');
 		$this->twig = new Twig_Environment($loader);
+		$data['uri'] = $this->request->uri();
 		return $this->twig->render($view, $data);
 	}
 
