28c7acd56b2c1e8c6185a908d507ec9a22eaf077
GrygrFlzr <grygrflzr@hotmail.com>
2014-08-04 19:02:40 +0800

    Updated Version page to show more data

--- app/controllers/VersionsController.php
+++ app/controllers/VersionsController.php
@@ -14,6 +14,24 @@ class VersionsController extends Controller\Twig {
 	{
 		$version = Version::where('version', $version)->first();
 		$mods = $version->mods()->get();
+		
+		foreach($mods as &$mod) {
+			$authors = $mod->authors()->get();
+			foreach($authors as &$author) {
+				$mod['authors'][] = $author->author()->get();
+			}
+			
+			$dependencies = $mod->dependencies()->get();
+			foreach($dependencies as &$dependency) {
+				$mod['dependencies'][] = $dependency->dependency()->get();
+			}
+			
+			$types = $mod->types()->get();
+			foreach($types as &$types) {
+				$mod['types'][] = $types->type()->get();
+			}
+		}
+		
 		return $this->make('versions/version.twig', compact('version', 'mods'));
 	}
 
