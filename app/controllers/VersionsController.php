adc08ceee443f91010f3d78a08f59bf062471028
GrygrFlzr <grygrflzr@hotmail.com>
2014-08-04 20:25:59 +0800

    Eager loading with eloquent rather than using for-loops

--- app/controllers/VersionsController.php
+++ app/controllers/VersionsController.php
@@ -13,24 +13,7 @@ class VersionsController extends Controller\Twig {
 	public function getVersion($version)
 	{
 		$version = Version::where('version', $version)->first();
-		$mods = $version->mods()->get();
-		
-		foreach($mods as &$mod) {
-			$authors = $mod->authors()->get();
-			foreach($authors as &$author) {
-				$mod['authors'][] = $author->author()->get();
-			}
-			
-			$dependencies = $mod->dependencies()->get();
-			foreach($dependencies as &$dependency) {
-				$mod['dependencies'][] = $dependency->dependency()->get();
-			}
-			
-			$types = $mod->types()->get();
-			foreach($types as &$types) {
-				$mod['types'][] = $types->type()->get();
-			}
-		}
+		$mods = $version->mods()->with('authors.author','dependencies.dependency','types.type')->get();
 		
 		return $this->make('versions/version.twig', compact('version', 'mods'));
 	}
